4442e4616f2be50d5bada2a61a5fe760fac430cd
Tam309 <t3doch00@students.oamk.fi>
2024-11-22 16:16:44 +0200

    minor fix

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { postRegister, postLogin, confirmUser } from "../controllers/userController.js";
+import { postRegister, postLogin, confirmUser } from "../controllers/UserController.js";
 
 const userRouter = express.Router();
 
